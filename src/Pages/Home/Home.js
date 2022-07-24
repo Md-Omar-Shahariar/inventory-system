446cdbb3f5905c8f5e4c3c2bcaf73cfc05861c97
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-07-24 19:44:52 +0600

    testimonial

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,6 +4,7 @@ import Bannner2 from "./Bannner2";
 import Info from "./Info";
 import MakeAppointment from "./MakeAppointment";
 import Services from "./Services";
+import Testimonials from "./Testimonials";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Services></Services>
       <Bannner2></Bannner2>
       <MakeAppointment></MakeAppointment>
+      <Testimonials></Testimonials>
     </div>
   );
 };
